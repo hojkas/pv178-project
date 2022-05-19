05cc920ac5aadb244a7b0a89763cbfd101637023
hojkas <ivka.strnadova@gmail.com>
2022-05-19 21:34:13 +0200

    show tournament players

--- tournament_manager_frontend/src/App.js
+++ tournament_manager_frontend/src/App.js
@@ -2,10 +2,14 @@ import React, { useState } from "react";
 import { API_BASE_URL } from "./utilities/constants";
 import { format } from "date-fns";
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faAngleDown, faAngleUp } from '@fortawesome/free-solid-svg-icons'
+
 export default function App() {
   const [tournaments, SetTournaments] = useState([]);
   const [selectedTournament, SetTournament] = useState();
-  
+  const [tournamentPlayers, SetTournamentPlayers] = useState([]);
+
 
   function getTournaments() {
     const url = API_BASE_URL + "Tournaments";
@@ -14,13 +18,34 @@ export default function App() {
     })
     .then(response => response.json())
     .then(tournamentsFromServer => {
-      console.log(tournaments);
+      console.log(tournamentsFromServer);
       SetTournaments(tournamentsFromServer);
     })
     .catch((error) => {
       console.log(error);
       alert(error);
     });
+  }
+
+  function getTournamentPlayers() {
+    const url = API_BASE_URL + "Tournaments/" + selectedTournament.id + '/Players';
+    fetch(url, {
+      method: 'GET'
+    })
+    .then(response => response.json())
+    .then(playersFromServer => {
+      console.log(playersFromServer);
+      SetTournamentPlayers(playersFromServer);
+    })
+    .catch((error) => {
+      console.log(error);
+      alert(error);
+    });
+  }
+
+  function setNewTournament(tournament) {
+    SetTournament(tournament);
+    SetTournamentPlayers([]);
   }
 
   return (
@@ -33,7 +58,11 @@ export default function App() {
       {tournaments.length > 0 && renderTournamentsManipulationButton()}
       {tournaments.length > 0 && renderTournamentsTable()}
 
-      {selectedTournament != null && renderTounament()}
+      {selectedTournament != null &&
+        renderTounament() &&
+        renderTournamentPlayersDiv()
+      }
+
     </div>
   );
 
@@ -73,7 +102,7 @@ export default function App() {
                 <td>{tournament.name}</td>
                 <td>{tournament.description}</td>
                 <td>
-                  <button className="btn-small btn-blue" onClick={() => SetTournament(tournament)}>Select this tournament</button>
+                  <button className="btn-small btn-blue" onClick={() => setNewTournament(tournament)}>Select this tournament</button>
                 </td>
               </tr>
             ))}
@@ -104,6 +133,42 @@ export default function App() {
             "No date given"}
         </div>
       </div>
-    );
+    )
   }
+
+  function renderTournamentPlayersDiv() {
+    if(tournamentPlayers.length == 0) return (
+      <div className="header2 clickable" onClick={getTournamentPlayers}>
+        Players <FontAwesomeIcon icon={faAngleDown}/>
+      </div>
+    )
+    else return (
+      <div>
+        <div className="header2 clickable" onClick={() => SetTournamentPlayers([])}>
+          Players <FontAwesomeIcon icon={faAngleUp}/>
+        </div>
+        <div>
+          <table className="tournament-table simple-table">
+            <thead>
+              <tr>
+                <th scope="col">Id</th>
+                <th scope="col">Name</th>
+                <th scope="col">Note</th>
+              </tr>
+            </thead>
+            <tbody>
+              {tournamentPlayers.map((player) => (
+                <tr key={player.id}>
+                  <th scope="row">{player.id}</th>
+                  <td>{player.name}</td>
+                  <td>{player.note}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    )
+  }
+  
 }
