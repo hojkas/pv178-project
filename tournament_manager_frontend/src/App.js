9b5285b78cee1394ed8f9f1377fd3139ca7800d1
hojkas <ivka.strnadova@gmail.com>
2022-05-19 20:57:31 +0200

    show tournament

--- tournament_manager_frontend/src/App.js
+++ tournament_manager_frontend/src/App.js
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
 import { API_BASE_URL } from "./utilities/constants";
+import { format } from "date-fns";
 
 export default function App() {
   const [tournaments, SetTournaments] = useState([]);
-  const [loaded_tournament, SetTournament] = useState([]);
+  const [selectedTournament, SetTournament] = useState();
+  
 
   function getTournaments() {
     const url = API_BASE_URL + "Tournaments";
@@ -22,15 +24,16 @@ export default function App() {
   }
 
   return (
-    <div className="App md:container md:mx-auto mt-5">
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
+    <div className="App md:container md:mx-auto mt-5 px-5">
+      <h1 className="text-3xl font-bold text-blue-800">
+        My very not scuffed Tournament Manager
       </h1>
 
-      {tournaments.length == 0 && renderLoadTournamentsButton()}
+      {tournaments.length === 0 && renderLoadTournamentsButton()}
       {tournaments.length > 0 && renderTournamentsManipulationButton()}
       {tournaments.length > 0 && renderTournamentsTable()}
 
+      {selectedTournament != null && renderTounament()}
     </div>
   );
 
@@ -51,8 +54,9 @@ export default function App() {
   }
 
   function renderTournamentsTable() {
-    return(
+    return (
       <div className="mt-5">
+        <div className="header2">Tournaments</div>
         <table className="tournament-table simple-table">
           <thead>
             <tr>
@@ -65,11 +69,11 @@ export default function App() {
           <tbody>
             {tournaments.map((tournament) => (
               <tr key={tournament.id}>
-                <td scope="row">{tournament.id}</td>
+                <th scope="row">{tournament.id}</th>
                 <td>{tournament.name}</td>
                 <td>{tournament.description}</td>
                 <td>
-                  <button className="btn-small btn-blue">Load this tournament</button>
+                  <button className="btn-small btn-blue" onClick={() => SetTournament(tournament)}>Select this tournament</button>
                 </td>
               </tr>
             ))}
@@ -77,5 +81,29 @@ export default function App() {
         </table>
       </div>
     )
+  }
+
+  function renderTounament() {
+    return (
+      <div className="mt-5">
+        <div className="header2">Tournament info</div>
+        <div>Id: {selectedTournament.id}</div>
+        <div>{selectedTournament.name}</div>
+        <div>{selectedTournament.description}</div>
+        
+        <div>
+          Start time:&nbsp;
+            {selectedTournament.startDate ?
+            format(new Date(selectedTournament.startDate), "dd.MM.yyyy H:mm") :
+            "No date given"}
+        </div>
+        <div>
+          End time:&nbsp;
+            {selectedTournament.endDate ?
+            format(new Date(selectedTournament.endDate), "dd.MM.yyyy H:mm") :
+            "No date given"}
+        </div>
+      </div>
+    );
   }
 }
