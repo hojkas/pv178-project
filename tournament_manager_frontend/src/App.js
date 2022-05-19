757cf71b91173746cf0d2d276d6ae0717130fbb7
hojkas <ivka.strnadova@gmail.com>
2022-05-19 14:57:06 +0200

    loading tournaments basic done

--- tournament_manager_frontend/src/App.js
+++ tournament_manager_frontend/src/App.js
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
+import { API_BASE_URL } from "./utilities/constants";
 
 export default function App() {
   const [tournaments, SetTournaments] = useState([]);
+  const [loaded_tournament, SetTournament] = useState([]);
 
   function getTournaments() {
-    const url = "https://localhost:7191/api/Tournaments";
+    const url = API_BASE_URL + "Tournaments";
     fetch(url, {
       method: 'GET'
     })
@@ -25,28 +27,53 @@ export default function App() {
         Hello world!
       </h1>
 
-      <div className="mt-5">
-        <button onClick={getTournaments} className="btn btn-blue">Load Tournaments</button>
-      </div>
+      {tournaments.length == 0 && renderLoadTournamentsButton()}
+      {tournaments.length > 0 && renderTournamentsManipulationButton()}
+      {tournaments.length > 0 && renderTournamentsTable()}
 
-      <div className="mt-5">
-        {renderTournamentsTable()}
-      </div>
     </div>
   );
 
+  function renderLoadTournamentsButton() {
+    return (
+      <div className="mt-5">
+        <button onClick={getTournaments} className="btn btn-blue">Load Tournaments</button>
+      </div>
+    );
+  }
+
+  function renderTournamentsManipulationButton() {
+    return (
+      <div className="mt-5">
+        <button onClick={getTournaments} className="btn btn-blue">Reload Tournaments</button>
+      </div>
+    );
+  }
 
   function renderTournamentsTable() {
     return(
-      <div className="tournament-table">
-        <table className="table-auto border-collapse border border-slate-600">
+      <div className="mt-5">
+        <table className="tournament-table simple-table">
           <thead>
             <tr>
-              <th className="border border-slate-600" scope="col">Id</th>
-              <th className="border border-slate-600" scope="col">Name</th>
-              <th className="border border-slate-600" scope="col">Description</th>
+              <th scope="col">Id</th>
+              <th scope="col">Name</th>
+              <th scope="col">Description</th>
+              <th scope="col"></th>
             </tr>
           </thead>
+          <tbody>
+            {tournaments.map((tournament) => (
+              <tr key={tournament.id}>
+                <td scope="row">{tournament.id}</td>
+                <td>{tournament.name}</td>
+                <td>{tournament.description}</td>
+                <td>
+                  <button className="btn-small btn-blue">Load this tournament</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
         </table>
       </div>
     )
